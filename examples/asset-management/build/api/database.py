ed361856b6633ad41311544354d86d98fa24a2e5
Tom Softreck <tom@sapletta.com>
2026-04-16 20:54:59 +0200

    refactoring

--- examples/asset-management/build/api/database.py
+++ examples/asset-management/build/api/database.py
@@ -2,7 +2,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, DeclarativeBase
 
-DATABASE_URL = "sqlite:///./data.db"
+DATABASE_URL = "postgresql://oqlos:changeme@db:5432/scba"
 
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False} if "sqlite" in DATABASE_URL else {})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
